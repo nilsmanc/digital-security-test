e58b23281424a6fcf490c7e68badc4481bf46f13
nilsmanc <etereaiberia@gmail.com>
2022-09-18 19:27:26 +0300

    logic, layout

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [amountFrom, setAmountFrom] = useState(1)
   const [amountTo, setAmountTo] = useState(1)
   const [currencyFrom, setCurrencyFrom] = useState('USD')
-  const [currencyTo, setCurrencyTo] = useState('USD')
+  const [currencyTo, setCurrencyTo] = useState('EUR')
   const [rates, setRates] = useState([])
 
   useEffect(() => {
@@ -17,18 +17,50 @@ function App() {
         setRates(response.data.rates)
       })
   }, [])
+
+  useEffect(() => {
+    if (!!rates) {
+      handleAmountFromChange(1)
+    }
+  }, [rates])
+
+  const format = (number) => {
+    return number.toFixed(4)
+  }
+
+  const handleAmountFromChange = (amountFrom) => {
+    setAmountTo(format((amountFrom * rates[currencyTo]) / rates[currencyFrom]))
+    setAmountFrom(amountFrom)
+  }
+
+  const handleCurrencyFromChange = (currencyFrom) => {
+    setAmountTo(format((amountFrom * rates[currencyTo]) / rates[currencyFrom]))
+    setCurrencyFrom(currencyFrom)
+  }
+
+  const handleAmountToChange = (amountTo) => {
+    setAmountFrom(format((amountTo * rates[currencyFrom]) / rates[currencyTo]))
+    setAmountTo(amountTo)
+  }
+
+  const handleCurrencyToChange = (currencyTo) => {
+    setAmountFrom(format((amountTo * rates[currencyFrom]) / rates[currencyTo]))
+    setCurrencyTo(currencyTo)
+  }
+
   return (
     <div>
+      <h1>Currency converter</h1>
       <CurrencyInput
-        onAmountChange={setAmountFrom}
-        onCurrencyChange={setCurrencyFrom}
+        onAmountChange={handleAmountFromChange}
+        onCurrencyChange={handleCurrencyFromChange}
         currencies={Object.keys(rates)}
         amount={amountFrom}
         currency={currencyFrom}
       />
       <CurrencyInput
-        onAmountChange={setAmountTo}
-        onCurrencyChange={setCurrencyTo}
+        onAmountChange={handleAmountToChange}
+        onCurrencyChange={handleCurrencyToChange}
         currencies={Object.keys(rates)}
         amount={amountTo}
         currency={currencyTo}
