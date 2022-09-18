e8cd8f1af05703ccb9a976f352e8fed54f468312
nilsmanc <etereaiberia@gmail.com>
2022-09-18 16:41:54 +0300

    api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { CurrencyInput } from './CurrencyInput'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
+  const [amountFrom, setAmountFrom] = useState(1)
+  const [amountTo, setAmountTo] = useState(1)
+  const [currencyFrom, setCurrencyFrom] = useState('USD')
+  const [currencyTo, setCurrencyTo] = useState('USD')
+  const [rates, setRates] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://api.apilayer.com/fixer/latest?base=USD&apikey=vxc9aAtpVA6uSmfQl4HvpJEP6dBPpJnm')
+      .then((response) => {
+        setRates(response.data.rates)
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CurrencyInput
+        onAmountChange={setAmountFrom}
+        onCurrencyChange={setCurrencyFrom}
+        currencies={Object.keys(rates)}
+        amount={amountFrom}
+        currency={currencyFrom}
+      />
+      <CurrencyInput
+        onAmountChange={setAmountTo}
+        onCurrencyChange={setCurrencyTo}
+        currencies={Object.keys(rates)}
+        amount={amountTo}
+        currency={currencyTo}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
